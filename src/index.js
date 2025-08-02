5b60b6af6cd6e6d26723974be2753a10a3512941
Ramiro <135643361+matvera0608@users.noreply.github.com>
2025-08-02 15:05:25 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import express from 'express'
+import usuarioRouter from './routes/usuario.routes.js'
+import libroRouter from './routes/libro.routes.js'
+import prestamoRouter from './routes/préstamo.routes.js'
+import resenaRouter from './routes/reseña.routes.js'
 const app = express()
 const port = 8080
 
 app.use(express.json())
-
+// Usar los routers
+app.use('/usuario', usuarioRouter)
+app.use('/libro', libroRouter)
+app.use('/préstamo', prestamoRouter)
+app.use('/reseña', resenaRouter)
 //#region TODOS LOS ENDPOINTS
 app.get('/', (req, res) => {
   res.send('¡Bienvenido al servidor Express con todos los endpoints de la consigna!')
